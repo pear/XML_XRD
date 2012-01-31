39fbba1945e199bbebc439745ac06943715af3a4
Christian Weiske <cweiske@cweiske.de>
2012-01-31 22:34:53 +0100

    more tests: xrd properties

--- tests/XML/XRDTest.php
+++ tests/XML/XRDTest.php
@@ -179,6 +179,63 @@ XRD;
         }
         $this->assertEquals('http://example.com/cv.html', $links[0]->href);
     }
+
+    public function testArrayAccess()
+    {
+        $this->xrd->loadFile(__DIR__ . '/../properties.xrd');
+        $this->assertEquals('Stevie', $this->xrd['name']);
+        $this->assertEquals('green', $this->xrd['color']);
+        $this->assertNull($this->xrd['empty']);
+        $this->assertNull($this->xrd['doesnotexist']);
+    }
+
+    public function testArrayAccessNull()
+    {
+        $this->xrd->loadFile(__DIR__ . '/../properties.xrd');
+        $this->assertNull($this->xrd['empty']);
+        $this->assertNull($this->xrd['doesnotexist']);
+    }
+
+    public function testArrayAccessDoesNotExist()
+    {
+        $this->xrd->loadFile(__DIR__ . '/../properties.xrd');
+        $this->assertFalse(isset($this->xrd['doesnotexist']));
+        $this->assertNull($this->xrd['doesnotexist']);
+    }
+
+    public function testGetPropertiesAll()
+    {
+        $this->xrd->loadFile(__DIR__ . '/../properties.xrd');
+        $props = array();
+        foreach ($this->xrd->getProperties() as $property) {
+            $this->assertInstanceOf('XML_XRD_Element_Property', $property);
+            $props[] = $property;
+        }
+        $this->assertEquals(6, count($props));
+        
+        $this->assertEquals('name', $props[0]->type);
+        $this->assertEquals('Stevie', $props[0]->value);
+        
+        $this->assertEquals('color', $props[2]->type);
+        $this->assertEquals('orange', $props[2]->value);
+    }
+
+    public function testGetPropertiesType()
+    {
+        $this->xrd->loadFile(__DIR__ . '/../properties.xrd');
+        $props = array();
+        foreach ($this->xrd->getProperties('color') as $property) {
+            $this->assertInstanceOf('XML_XRD_Element_Property', $property);
+            $props[] = $property;
+        }
+        $this->assertEquals(2, count($props));
+        
+        $this->assertEquals('color', $props[0]->type);
+        $this->assertEquals('green', $props[0]->value);
+
+        $this->assertEquals('color', $props[1]->type);
+        $this->assertEquals('orange', $props[1]->value);
+    }
 }
 
 ?>
