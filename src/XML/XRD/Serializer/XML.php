8c036343b0f43282652b9ecca4e247a3c1d57d77
Christian Weiske <cweiske@cweiske.de>
2012-02-08 23:32:01 +0100

    fix xml serializer

--- src/XML/XRD/Serializer/XML.php
+++ src/XML/XRD/Serializer/XML.php
@@ -35,8 +35,10 @@ class XML_XRD_Serializer_XML
         $x = new XMLWriter();
         $x->openMemory();
         $x->startDocument('1.0', 'UTF-8');
+        $x->setIndent(true);
         $x->startElement('XRD');
         $x->writeAttribute('xmlns', 'http://docs.oasis-open.org/ns/xri/xrd-1.0');
+        $x->writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
 
         $x->writeElement('Subject', $this->xrd->subject);
         foreach ($this->xrd->aliases as $alias) {
@@ -51,7 +53,7 @@ class XML_XRD_Serializer_XML
         }
 
         foreach ($this->xrd->links as $link) {
-            $x->writeElement('Link');
+            $x->startElement('Link');
             $x->writeAttribute('rel', $link->rel);
             if ($link->type !== null) {
                 $x->writeAttribute('type', $link->type);
@@ -65,17 +67,17 @@ class XML_XRD_Serializer_XML
             }
 
             foreach ($link->titles as $lang => $value) {
-                $x->writeElement('Title', $value);
+                $x->startElement('Title');
                 if ($lang) {
-                    $x->writeAttributeNS(
-                        'xml', 'lang', 'http://www.w3.org/XML/1998/namespace',
-                        'true'
-                    );
+                    $x->writeAttribute('xml:lang', $lang);
                 }
+                $x->text($value);
+                $x->endElement();
             }
             foreach ($link->properties as $property) {
                 $this->writeProperty($x, $property);
             }
+            $x->endElement();
         }
 
         $x->endElement();
@@ -86,16 +88,14 @@ class XML_XRD_Serializer_XML
     protected function writeProperty(
         XMLWriter $x, XML_XRD_Element_Property $property
     ) {
-        if ($property->value === null) {
-            $x->writeElement('Property');
-            $x->writeAttributeNS(
-                'xsi', 'nil', 'http://www.w3.org/2001/XMLSchema-instance',
-                'true'
-            );
-        } else {
-            $x->writeElement('Property', $property->value);
-        }
+        $x->startElement('Property');
         $x->writeAttribute('type', $property->type);
+        if ($property->value === null) {
+            $x->writeAttribute('xsi:nil', 'true');
+        } else {
+            $x->text($property->value);
+        }
+        $x->endElement();
     }
 }
 
