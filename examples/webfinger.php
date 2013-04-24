2130869df20a35f9d6d6317e460641f843d276e8
Christian Weiske <cweiske@cweiske.de>
2013-04-24 16:13:53 +0200

    update webfinger client to latest webfinger draft

--- examples/webfinger.php
+++ examples/webfinger.php
@@ -3,36 +3,30 @@
  * Basic WebFinger implementation to discover a user's OpenID provider
  * from just his email address
  */
-$email = 'cweiske@cweiske.de';
+if ($argc < 2) {
+    echo "Usage: $argv[0] user@example.com\n";
+    exit(1);
+}
+$email = $argv[1];
 
 $host = substr($email, strpos($email, '@') + 1);
 
 require_once 'XML/XRD.php';
 $xrd = new XML_XRD();
 try {
-    $xrd->loadFile('http://' . $host . '/.well-known/host-meta');
+    $xrd->loadFile(
+        'http://' . $host . '/.well-known/webfinger?resource=acct:' . $email,
+        'json'
+    );
 } catch (XML_XRD_Exception $e) {
-    die('Loading XRD file failed: '  . $e->getMessage());
+    echo 'Loading JRD file failed: '  . $e->getMessage() . "\n";
+    exit(1);
 }
 
-$link = $xrd->get('lrdd', 'application/xrd+xml');
-if ($link === null) {
-    die('No LRDD link found');
-}
-$template = $link->template;
-$lrddUri = str_replace('{uri}', urlencode('acct:' . $email), $template);
-echo 'URL with infos about ' . $email . ' is ' . $lrddUri . "\n";
-
-$xrdLrdd = new XML_XRD();
-try {
-    $xrdLrdd->loadFile($lrddUri);
-} catch (XML_XRD_Exception $e) {
-    die('Loading LRDD XRD file failed: '  . $e->getMessage());
-}
-
-$openIdLink = $xrdLrdd->get('http://specs.openid.net/auth/2.0/provider');
+$openIdLink = $xrd->get('http://specs.openid.net/auth/2.0/provider');
 if ($openIdLink === null) {
-    die("No OpenID provider found for $email\n");
+    echo "No OpenID provider found for $email\n";
+    exit(2);
 }
 
 echo $email . '\'s OpenID provider is: ' . $openIdLink->href . "\n";
